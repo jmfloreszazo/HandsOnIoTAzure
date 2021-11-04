659dc8f32ff015c8ea11c1ed989200b9b9586fe9
Jose Maria Flores Zazo <jose.flores@tokiota.com>
2021-11-04 09:33:05 +0100

    Add test class

---
+++ csharpConsoleApp/csharpConsoleApp/test/TestAClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace csharpConsoleApp.test
+{
+    public class TestA
+    {
+        private TestA()
+        {
+            var measurement = 7.5;
+
+            switch (measurement)
+            {
+                case < 0.0:
+                    Console.WriteLine($"Measured value is {measurement}; too low.");
+                    break;
+
+                case > 15.0:
+                    Console.WriteLine($"Measured value is {measurement}; too high.");
+                    break;
+
+                case double.NaN:
+                    Console.WriteLine("Failed measurement.");
+                    break;
+
+                default:
+                    Console.WriteLine($"Measured value is {measurement}.");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ csharpConsoleApp/csharpConsoleApp/test/TestBClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace csharpConsoleApp.test
+{
+    public class TestB
+    {
+        private TestB()
+        {
+            var measurement = 7.5;
+
+            if (measurement < 0.0)
+            {
+                Console.WriteLine($"Measured value is {measurement}; too low.");
+            }
+            else if (measurement > 15.0)
+            {
+                Console.WriteLine($"Measured value is {measurement}; too high.");
+            }
+            else if (double.IsNaN(measurement))
+            {
+                Console.WriteLine("Failed measurement.");
+            }
+            else
+            {
+                Console.WriteLine($"Measured value is {measurement}.");
+            }
+        }
+
+    }
+}
