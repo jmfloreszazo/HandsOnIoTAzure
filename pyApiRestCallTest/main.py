9f2d170c682a0c61190ef7f169803bea8cf72693
Jose Maria Flores Zazo <jose.flores@tokiota.com>
2021-11-05 09:33:33 +0100

    Add Request test projects

---
+++ pyApiRestCallTest/main.py
@@ -0,0 +1,55 @@
+import requests
+from plotly.graph_objs import Bar
+from plotly import offline
+
+url = "https://pyapirestcalltest-8082.restdb.io/rest/requests"
+
+headers = {
+    'content-type': "application/json",
+    'x-apikey': "b8e7133aca0c1cbdf661d147eb533874de21f",
+    'cache-control': "no-cache"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+print("Show data in text format.")
+print("-------------------------")
+
+print(response.text)
+
+responseJSON = response.json()
+
+print("Show total value.")
+print("-----------------")
+
+totalValue = 0
+
+for item in responseJSON:
+    totalValue += item["value"]
+
+print(totalValue)
+
+print("Create Bar chart.")
+print("-----------------")
+
+names, values = [], []
+
+for item in responseJSON:
+    names.append(item["name"])
+    values.append(item["value"])
+
+data = [{
+    "type": "bar",
+    "x": names,
+    "y": values, }]
+
+myLayout = {
+    "title": "Price of companies in $ millions",
+    "xaxis": {"title": "Company"},
+    "yaxis": {"title": "Value"}, }
+
+fig = {
+    "data": data,
+    "layout": myLayout}
+
+offline.plot(fig, filename='pySampleReport.html')
---
+++ pyApiRestCallTest/readme.md
@@ -0,0 +1,6 @@
+# Install packages
+
+```
+pip install requests
+pip install plotly
+```
